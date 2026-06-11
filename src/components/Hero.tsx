19926067270bdcbd48eda98fcd5a235cf850e542
Ali Abdulmaksoud <aabdulma@uoguelph.ca>
2026-06-11 06:35:06 -0400

    final spot for scroll logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -99,7 +99,7 @@ export default function Hero() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5, duration: 1 }}
-          className="absolute -bottom-8 left-1/2 -translate-x-1/2"
+          className="absolute -bottom-48 left-1/2 -translate-x-1/2"
         >
           <motion.div
             animate={{ y: [0, 8, 0] }}
